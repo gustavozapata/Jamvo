7efea7c61b40e3fd6c83d3054c399295eee89012
Gustavo Zapata <gustavozapataq@hotmail.com>
2020-01-04 23:01:57 +0000

    first commit

--- Jamvo/ViewController.swift
+++ Jamvo/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var texto: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btn(_ sender: Any) {
+        texto.isHidden = false
+    }
+    
 }
 
